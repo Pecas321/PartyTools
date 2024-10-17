5bc42bd81b411e88cab706f34f700271e9d7dc72
Pecas321 <danielc.salinasr@utadeo.edu.co>
2024-10-17 18:34:13 -0500

    commit dani 3

--- app/src/main/java/com/example/partytools/MainActivity.kt
+++ app/src/main/java/com/example/partytools/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.partytools
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonIniciarSesion = findViewById<Button>(R.id.iniciarsesion_inicio)
+        buttonIniciarSesion.setOnClickListener {
+            val intent = Intent(this, InicioSesion::class.java)
+            startActivity(intent)
         }
     }
 }
