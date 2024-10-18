048b4608347b2c59ac25671b46299a3a7e36c501
Pecas321 <danielc.salinasr@utadeo.edu.co>
2024-10-17 19:02:59 -0500

    commit juegosdemesa brayan

--- app/src/main/java/com/example/partytools/Juegosdemesa.kt
+++ app/src/main/java/com/example/partytools/Juegosdemesa.kt
@@ -1,26 +1,20 @@
 package com.example.partytools
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class InicioSesion : AppCompatActivity() {
+class Juegosdemesa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_inicio_sesion)
+        setContentView(R.layout.activity_juegosdemesa)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val volver_iniciosesion = findViewById<Button>(R.id.volver_iniciosesion)
-        volver_iniciosesion.setOnClickListener {
-            val intent = Intent(this, InicioSesion::class.java)
-            startActivity(intent)
-        }
+    }
 }
